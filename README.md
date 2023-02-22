e9325c36f5560918a3da2482745e61bf0d102061
rasul <rasul.iskandarov@ut.ee>
2023-02-22 13:18:53 +0200

    set up backend APIs to handle DB requests

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Exercise-Tracker
-Web Application with following technologies: MongoDB, NodeJS (+express), React, Mongoose 
+Web Application with following technologies: 
+MongoDB (+ Mongoose)
+NodeJS (+ express, cors, dotenv)
+React (+ Bootstrap)
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const exerciseRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
 require('dotenv').config();
 
@@ -11,13 +13,17 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.set('strictQuery', false);
+mongoose.connect(uri, { useNewUrlParser: true });
 
 const connection = mongoose.connection;
 
 connection.once('open', () => {
   console.log('MongoDB database connection established succesfully');
 });
+
+app.use('/exercises', exerciseRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
